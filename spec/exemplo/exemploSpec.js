ab9f45ed3f9f5bb2966f5e02a6b108ad43515263
Raul <raul.cfr112@gmail.com>
2024-04-18 11:25:35 -0400

    Instalado e configurado o karma.

--- spec/exemplo/exemploSpec.js
+++ spec/exemplo/exemploSpec.js
@@ -4,4 +4,8 @@ describe('Suite de teste de exemplo com nodeJS', function () {
         expect(true).toEqual(true);
     });
 
+    it('deve demonstrar o uso do karma em modo de escuta', () => {
+        expect(1).toEqual(1);
+    });
+
 });
